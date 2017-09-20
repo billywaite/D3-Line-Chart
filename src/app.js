c8c295f8ed0289bfc9864df41cc0dcc429b53ed5
Billy Waite <wwaite@execonline.com>
2017-09-20 14:48:43 -0400

    Created basic line chart

---
+++ src/app.js
@@ -0,0 +1,97 @@
+var margin = { top: 10, right: 20, bottom: 30, left: 30 };
+var width = 400 - margin.left - margin.right;
+var height = 565 - margin.top - margin.bottom;
+
+var svg = d3.select('.chart')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .call(responsivefy)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+d3.json('data/data.json', function (err, data) {
+
+  // Function to parse time
+  var parseTime = d3.timeParse('%Y/%m/%d');
+
+  // Parse the date and close
+  data.forEach(company => {
+    company.values.forEach(d => {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+    })
+  })
+
+  // Create linear scale for the Y axis using close
+  var yScale = d3.scaleLinear()
+    .domain([
+      d3.min(data, co => d3.min(co.values, d => d.close)),
+      d3.max(data, co => d3.max(co.values, d => d.close))
+    ])
+    .range([height, 0]);
+  svg
+    .append('g')
+    .call(d3.axisLeft(yScale));
+
+  // Create time scale for the X axis using date
+  var xScale = d3.scaleTime()
+    .domain([
+      d3.min(data, co => d3.min(co.values, d => d.date)),
+      d3.max(data, co => d3.max(co.values, d => d.date))
+    ])
+    .range([0, width]);
+  svg
+    .append('g')
+      .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale).ticks(5));
+
+  // Function to create the lines
+  var line = d3.line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.close))
+    .curve(d3.curveCatmullRom.alpha(0.5));
+
+  // Append the lines to the svg element and add styles
+  svg
+    .selectAll('.line')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('class', 'line')
+    .attr('d', d => line(d.values))
+    .style('stroke', (d, i) => ['#FF9900', '#3369E8'][i])
+    .style('stroke-width', 2)
+    .style('fill', 'none');
+
+});
+
+
+
+// Function for responsive resizing
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width")),
+      height = parseInt(svg.style("height")),
+      aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+      .attr("preserveAspectRatio", "xMinYMid")
+      .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+      var targetWidth = parseInt(container.style("width"));
+      svg.attr("width", targetWidth);
+      svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
